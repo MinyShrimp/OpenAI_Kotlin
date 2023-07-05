f795073f3fb2bfe0f0bd880f1bdf3a6eb7f7e735
MinyShrimp <ksk7584@gmail.com>
2023-07-05 15:35:50 +0900

    Lit에서 React로 변경

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,21 @@
 import * as path from "path";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    root: path.resolve(__dirname, 'src'),
+    mode: "development", // "development" | "production"
+    root: path.resolve(__dirname, "src"),
+    publicDir: path.resolve(__dirname, "public"),
+    build: {
+        outDir: path.resolve(__dirname, "dist"),
+    },
+    plugins: [react()],
     resolve: {
         alias: {
-            '~bootstrap': path.resolve(__dirname, "node_modules/bootstrap"),
-        }
+            "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+        },
     },
     server: {
         port: 3000,
-    }
+    },
 });
