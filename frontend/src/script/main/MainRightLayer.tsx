387df261a8c9f6f3326b6ef9b1e637b176509ab6
MinyShrimp <ksk7584@gmail.com>
2023-07-07 16:39:07 +0900

    ModelViewer Layer 추가

--- frontend/src/script/main/MainRightLayer.tsx
+++ frontend/src/script/main/MainRightLayer.tsx
@@ -1,16 +1,20 @@
 import {JSX} from "react";
 import {Box} from "@mui/material";
 import {Layer} from "../base/Layer";
-import {NewPromptLayer} from "../prompt/NewPromptLayer";
+import {ModelViewerLayer} from "../model_viewer/ModelViewerLayer";
 
 export default function MainRightLayer(): JSX.Element {
     return (
         <Box
             component={Layer}
             sx={{bgcolor: "background.secondary"}}
-            style={{width: "85%"}}
+            style={{
+                width: "85%",
+                overflow: "auto",
+            }}
         >
-            <NewPromptLayer/>
+            <ModelViewerLayer/>
+            {/*<NewPromptLayer/>*/}
         </Box>
     );
 }
---
+++ frontend/src/script/model_viewer/ModelViewerLayer.tsx
@@ -0,0 +1,52 @@
+import {JSX} from "react";
+
+import {DataGrid} from '@mui/x-data-grid';
+import {ModelList} from "./ModelListExample";
+import {Grid} from "@mui/material";
+
+export function ModelViewerLayer(): JSX.Element {
+    const getModels = () => {
+        return ModelList;
+    }
+
+    return (
+        <Grid
+            container
+            direction="column"
+            justifyContent="space-between"
+            alignItems="stretch"
+            height="100%"
+        >
+            <Grid item>
+                <h1>Model List</h1>
+            </Grid>
+            <Grid item height="90%" minHeight="520px">
+                <DataGrid
+                    style={{maxHeight: "100%"}}
+                    rows={getModels()}
+                    columns={[
+                        {field: 'id', headerName: 'ID', flex: 0.4, minWidth: 0},
+                        {field: 'object', headerName: 'Object', flex: 0.1, minWidth: 0},
+                        {
+                            field: 'created', headerName: 'Created', flex: 0.2, minWidth: 0,
+                            valueFormatter: params => {
+                                return new Date(params.value * 1000).toLocaleString();
+                            }
+                        },
+                        {field: 'owned_by', headerName: 'Owned_by', flex: 0.1, minWidth: 0},
+                        {field: 'root', headerName: 'Root', flex: 0.1, minWidth: 0},
+                        {field: 'parent', headerName: 'Parent', flex: 0.1, minWidth: 0},
+                    ]}
+                    initialState={{
+                        pagination: {
+                            paginationModel: {
+                                page: 0,
+                                pageSize: 20,
+                            },
+                        },
+                    }}
+                />
+            </Grid>
+        </Grid>
+    );
+}
